7575df6b4f98534ede8a8250c383e95e58adb5d6
Jorge <jorgevivar2005@gmail.com>
2025-04-03 10:04:16 +0200

    Update modified files

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,9 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-import { getDefaultConfig } from 'expo/metro-config'
+const { getDefaultConfig } = require('expo/metro-config')
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname)
 
 config.resolver.unstable_enablePackageExports = true
 
-export default config
+module.exports = config
