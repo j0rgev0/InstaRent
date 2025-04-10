d0faeaa4f5e808924c04ceb65bee4f532f3b6558
Jorge <jorgevivar2005@gmail.com>
2025-04-11 01:14:20 +0200

    update changes

--- metro.config.js
+++ metro.config.js
@@ -1,13 +1,13 @@
-const { getDefaultConfig } = require('expo/metro-config');
-const { withNativeWind } = require('nativewind/metro');
+const { getDefaultConfig } = require('expo/metro-config')
+const { withNativeWind } = require('nativewind/metro')
 
 /** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname)
 
 // Activamos soporte para "exports" modernos en los paquetes (como better-auth)
-config.resolver.unstable_enablePackageExports = true;
+config.resolver.unstable_enablePackageExports = true
 
 // Aplicamos NativeWind al final (esto también modifica el config)
-const finalConfig = withNativeWind(config, { input: './global.css' });
+const finalConfig = withNativeWind(config, { input: './global.css' })
 
-module.exports = finalConfig;
+module.exports = finalConfig
