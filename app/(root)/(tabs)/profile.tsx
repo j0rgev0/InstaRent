769d64276903eca0625ea9d211fa2868a670a764
Jorge <jorgevivar2005@gmail.com>
2025-04-11 16:46:08 +0200

    Update changes

--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -12,7 +12,7 @@ const ProfilePage = () => {
     <View className="flex-1 bg-gray-50 pt-10">
       <View className="bg-white p-5 items-center border-b border-gray-200">
         <View className="w-32 h-32 rounded-full bg-gray-100 justify-center items-center mb-4">
-          <Ionicons name="person-circle" size={100} color="#4A90E2" />
+          <Ionicons name="person-circle" size={100} color="#353949" />
         </View>
         <Text className="text-2xl font-bold text-gray-800 mb-2">{session?.user.name}</Text>
         <Text className="text-base text-gray-600">{session?.user.email}</Text>
@@ -20,7 +20,7 @@ const ProfilePage = () => {
       
       <View className="flex-1 p-5">
         <TouchableOpacity 
-          className="flex-row bg-blue-500 p-4 rounded-lg items-center justify-center mt-5"
+          className="flex-row bg-darkBlue p-4 rounded-lg items-center justify-center mt-5"
           onPress={() => authClient.signOut().then(() => router.replace("/"))}
         >
           <Ionicons name="log-out-outline" size={24} color="white" />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./app/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}', './pages/**/*.{js,jsx,ts,tsx}'],
-  
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        darkBlue: '#353949'
+      }
+    }
   },
   plugins: []
 }
