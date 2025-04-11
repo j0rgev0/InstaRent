373eabcc2d4535fb4c6e5017a005408b08f5b907
Jorge <jorgevivar2005@gmail.com>
2025-04-11 18:11:52 +0200

    Refactor tab layout styles and update tab bar appearance

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -1,18 +1,19 @@
 import { Tabs } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
+import { Platform, StyleSheet} from 'react-native';
 
 export default function RootLayout() {
   return (
-    <Tabs
+    <Tabs 
       screenOptions={{
-        tabBarActiveTintColor: "#000000",
+        tabBarActiveTintColor: "#353949",
+        tabBarStyle: styles.tab
       }}
     >
       <Tabs.Screen
         name="home"
         options={{
-          headerTitle: "Home",
-          tabBarLabel: "Home",
+          tabBarLabel: "",
           headerShown: false,
           tabBarIcon: ({ color, size, focused }) => (
             <Ionicons name={focused ? "home" : "home-outline"} color={color} size={size} />
@@ -22,8 +23,7 @@ export default function RootLayout() {
       <Tabs.Screen
         name="profile"
         options={{
-          headerTitle: "Profile",
-          tabBarLabel: "Profile",
+          tabBarLabel: " ",
           headerShown: false,
           tabBarIcon: ({ color, size, focused }) => (
             <Ionicons name={focused ? "person" : "person-outline"} color={color} size={size} />
@@ -33,3 +33,23 @@ export default function RootLayout() {
     </Tabs>
   );
 }
+
+
+const styles = StyleSheet.create({
+  tab: {
+    backgroundColor: 'rgba(255, 255, 255, 0.2)',
+    position: 'absolute',
+    borderRadius: 30,
+    paddingTop: 10,
+    height: Platform.OS === 'ios' ? 80 : 60,
+    borderColor: 'rgba(255, 255, 255, 0)',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  }
+})
